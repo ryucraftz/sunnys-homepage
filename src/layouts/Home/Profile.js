50ad47b2a256256f44c38d0cf9e1ec6ecfe22ecd
Kirito <vaibhavsathe0204@gmail.com>
2023-05-28 11:35:54 +0530

    initial commit

---
+++ src/layouts/Home/Profile.js
@@ -0,0 +1,100 @@
+import profileKatakana from 'assets/katakana-profile.svg?url';
+import profileImgLarge from 'assets/profile-large.jpg';
+import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
+import profileImg from 'assets/profile.jpg';
+import { Button } from 'components/Button';
+import { DecoderText } from 'components/DecoderText';
+import { Divider } from 'components/Divider';
+import { Heading } from 'components/Heading';
+import { Image } from 'components/Image';
+import { Link } from 'components/Link';
+import { Section } from 'components/Section';
+import { Text } from 'components/Text';
+import { Transition } from 'components/Transition';
+import { Fragment, useState } from 'react';
+import { media } from 'utils/style';
+import styles from './Profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi there" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      I’m Atharv,  the master of pixels and conqueror of realms. 
+      This is where epic tales and legendary victories reside.From slaying dragons to toppling empires, 
+      I've traversed countless virtual worlds, leaving a trail of triumph in my wake. Join My
+<Link href=""> Discord</Link> server and forge alliances with fellow warriors, strategize, and embark on daring quests together.
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {visible => (
+          <div className={styles.content}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="/contact"
+                icon="send"
+              >
+                Send me a message
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About Me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={[profileImg, profileImgLarge]}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me standing in front of the Torii on Miyajima, an island off the coast of Hiroshima in Japan"
+                />
+                <svg
+                  aria-hidden="true"
+                  width="135"
+                  height="765"
+                  viewBox="0 0 135 765"
+                  className={styles.svg}
+                  data-visible={visible}
+                >
+                  <use href={`${profileKatakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
