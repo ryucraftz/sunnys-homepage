1185a8c70ce45371695c13ee4db10c32ca0e1cfd
Kirito <vaibhavsathe0204@gmail.com>
2023-05-29 18:11:27 +0530

    initial commit

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -53,7 +53,7 @@ export const Profile = ({ id, visible, sectionRef }) => {
                 secondary
                 className={styles.button}
                 data-visible={visible}
-                href="/contact"
+                href="mailto:atharvshankaringle@gmail.com"
                 icon="send"
               >
                 Send me a message
