600aa3517d098038d07e8c4f58579f3738515d87
Kirito <vaibhavsathe0204@gmail.com>
2023-05-31 09:09:57 +0530

    initial commit

--- src/components/Monogram/Monogram.js
+++ src/components/Monogram/Monogram.js
@@ -27,6 +27,7 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
 <path d="M938.5 2175.5L1270.5 1534L1620.5 2175.5H938.5Z" fill="white"/>
 <path d="M1803 97H2485L2138.5 750L1803 97Z" fill="white"/>
 <path d="M2178.5 2562.5L1270.5 844.5L391.5 2562.5H570L1270.5 1183.5L2000 2562.5H2178.5ZM2178.5 2562.5H2558L1270.5 97L5 2562.5M938.5 2175.5L1270.5 1534L1620.5 2175.5H938.5ZM1803 97H2485L2138.5 750L1803 97ZM2415.5 2073.5V534.5L2138.5 1001.5L1886.5 534.5V1092.5L2393.5 2073.5H2415.5Z" stroke="black"/>
+       </g>
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
